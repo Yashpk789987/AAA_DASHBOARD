94860b14a7d96556937332f6bdca9210ac29e03a
yash <yashpk789987@gmail.com>
2020-02-15 10:03:05 +0530

    Updates

---
+++ precache-manifest.0d7d16e45873cdf3fefe4ec5afc40e69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3024a60bc53657e3647a",
+    "url": "/AAA_DASHBOARD/static/js/runtime~main.3024a60b.js"
+  },
+  {
+    "revision": "f441141afbe6556d1fd7",
+    "url": "/AAA_DASHBOARD/static/js/main.f441141a.chunk.js"
+  },
+  {
+    "revision": "ab5667069b884764724f",
+    "url": "/AAA_DASHBOARD/static/js/2.ab566706.chunk.js"
+  },
+  {
+    "revision": "f441141afbe6556d1fd7",
+    "url": "/AAA_DASHBOARD/static/css/main.de69c467.chunk.css"
+  },
+  {
+    "revision": "ab5667069b884764724f",
+    "url": "/AAA_DASHBOARD/static/css/2.c281264e.chunk.css"
+  },
+  {
+    "revision": "2e9334d2b00dac87ed188c8109ef0ed3",
+    "url": "/AAA_DASHBOARD/index.html"
+  }
+];
